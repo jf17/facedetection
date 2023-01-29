3b3515ec8050ccf4210155ad25248adb1e77d7bd
alex <javafirst17@gmail.com>
2023-01-30 01:28:28 +0300

    Add comment

--- webcam.py
+++ webcam.py
@@ -3,6 +3,7 @@ import cv2
 # Load the cascade
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
+# To capture video from webcam.
 cap = cv2.VideoCapture(0)
 
 while True:
